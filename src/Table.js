cecf2b43d37a26ae62295df0d7c6ffa429a9e250
Nihal <khanmohammednihal@gmail.com>
2023-02-05 13:52:40 +0530

    feat: Export PDF Functionality

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Grid, GridColumn } from "@progress/kendo-react-grid";
+import { Grid, GridColumn, GridToolbar } from "@progress/kendo-react-grid";
 import { orderBy } from "@progress/kendo-data-query";
-
+import { GridPDFExport } from "@progress/kendo-react-pdf";
 const initialSort = [
     {
       field: "engine_name",
@@ -11,8 +11,13 @@ const initialSort = [
   ];
 const Table = ({result}) => {
     const [sort, setSort] = React.useState(initialSort);
-    return (
-        <div style = {{width:"80%", margin:"auto"}}>
+    let gridPDFExport;
+  const exportPDF = () => {
+    if (gridPDFExport) {
+      gridPDFExport.save(result);
+    }
+  };
+    const grid = (
         <Grid
           style={{
             height: "400px",
@@ -24,11 +29,32 @@ const Table = ({result}) => {
         setSort(e.sort);
         }}
         >
+        <GridToolbar>
+        <button
+          title="Export PDF"
+          className="k-button k-button-md k-rounded-md k-button-solid k-button-solid-primary"
+          style={{width:"5rem"}}
+          onClick={exportPDF}
+        >
+          Export PDF
+        </button>
+      </GridToolbar>
           <GridColumn sortable = {true} field="engine_name" title="Engine Name" />
           <GridColumn sortable = {true} field="result" title="Result"/>
           <GridColumn sortable = {true} field="category" title="Category" />
         </Grid>
-        </div>
-      );
+    );
+        return (
+          <div>
+            {grid}
+            <GridPDFExport
+              ref={(pdfExport) => (gridPDFExport = pdfExport)}
+              paperSize="A4"
+              scale={0.8}
+            >
+              {grid}
+            </GridPDFExport>
+          </div>
+        );
 };
 export default Table;
